5f7fa83fc7c75c999b80c1dab5b74dda39456c55
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-03 06:59:53 +0100

    different solution

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,13 +1,26 @@
-def remove_duplicates(numbers):
+def withoutDuplicates(numbers):
     sameNumbers = numbers[:]
     noDuplicates = []
-    for digit in numbers:
+    for digit in sameNumbers:
         if digit not in noDuplicates:
             noDuplicates.append(digit)
         else:
             pass
     return noDuplicates
 
+print(withoutDuplicates([1, 2, 2, 3, 4, 6, 6, 6, 7, 8]))
+
+
+
+def remove_duplicates(numbers):
+    sameNumbers = numbers[:]
+    for digit in sameNumbers:
+        if numbers.count(digit) == 1:
+            continue
+        else:
+            numbers.remove(digit)
+    lessNumbers = numbers
+    return lessNumbers
+
 print(remove_duplicates([1, 2, 2, 3, 4, 6, 6, 6, 7, 8]))
-            
         
